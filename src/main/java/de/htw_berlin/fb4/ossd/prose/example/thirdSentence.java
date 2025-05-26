589b24496a5228c838dc2166cc3b146f7c28dbe1
Johanna Lange <johannalange@MacBook-Pro.local>
2025-05-26 12:28:32 +0200

    Ein neuen Satz von der Dichterin Sappho in der Klasse thirdSentence hinzugefügt

---
+++ src/main/java/de/htw_berlin/fb4/ossd/prose/example/thirdSentence.java
@@ -0,0 +1,10 @@
+package de.htw_berlin.fb4.ossd.prose.example;
+
+import de.htw_berlin.fb4.ossd.prose.Sentence;
+
+public class thirdSentence implements Sentence {
+    @Override
+    public String get() {
+        return "Although only breath, words which I command are immortal."; // (Sappho);
+    }
+}
