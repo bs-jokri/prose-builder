48d90cdeff24236e9dc6051d970089c48958e3af
Rawan-Alaswad <Rawan.AlAswad@Student.Htw-Berlin.de>
2025-06-04 13:26:27 +0200

    Add license headers to Java source files

--- src/main/java/de/htw_berlin/fb4/ossd/prose/Prose.java
+++ src/main/java/de/htw_berlin/fb4/ossd/prose/Prose.java
@@ -1,12 +1,5 @@
-/* SPDX-FileCopyrightText: 2025 Johannes Kristan
- *
- * See the NOTICE file(s) distributed with this work for additional
- * information regarding copyright ownership.
- *
- *  SPDX-License-Identifier: MIT
+/*
+ * Copyright (c) 2025 Johannes Kristan
+ * SPDX-License-Identifier: MIT
+ * See the LICENSE file in the root directory for more information.
  */
-package de.htw_berlin.fb4.ossd.prose;
-
-public interface Prose {
-    String get();
-}
